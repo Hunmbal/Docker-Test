73c63ab0d144b2b8b5cd966aeae06de1e27a7db2
Dania5567 <daniafarrukh@gmail.com>
2024-05-07 23:36:02 +0500

    project.py

--- Project/project.py
+++ Project/project.py
@@ -1,31 +1,29 @@
-import os
+import os 
 import requests
-from flask import Flask
-from flask import render_template
-
+from flask import Flask, request, render_template
 
 os.system("cls")
 api_key = "a4d611d55373c1d9b2e482ea62c67a91"
 
 app = Flask(__name__, static_url_path='/static')
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', weather=weather_data)
+    if request.method == 'POST':
+        city = request.form['city']
+        weather_data = get_weather(city)
+        return render_template('index.html', weather=weather_data)
+    else:
+        # Default city when the page is first loaded
+        default_city = "Karachi"
+        weather_data = get_weather(default_city)
+        return render_template('index.html', weather=weather_data)
 
 def get_weather(city):
     url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
-    #print(url)
     temp = requests.get(url)
     data = temp.json()
     return data
 
-
-if __name__ == "__main__":     
-    city = "karachi"  
-    weather_data = get_weather(city)
-    print(weather_data, "\n")
-
+if __name__ == "__main__":
     app.run(debug=True)
-
-
